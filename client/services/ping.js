8733fda7cd684157a66ce5c608f3105dccf4268f
unknown <joe@josephbrumaghim.com>
2015-11-25 14:38:03 -0500

    get status when we start running the service

--- client/services/ping.js
+++ client/services/ping.js
@@ -11,23 +11,24 @@ class Ping extends Service {
   }
 
   start() {
-    console.log(this)
+    this.getStatus();
+    setInterval(this.getStatus.bind(this), 10000);
+  }
+  getStatus() {
     var self = this;
-    setInterval(function() {
-      $.ajax({
-        url: self.url + "/status/" + self.get().ip,
+    $.ajax({
+      url: this.url + "/status/" + this.get().ip,
 
-      }).then(function(data) {
-        if(data.error) {
-          self.setField("status", data.error);
-          self.d3Render.value = "assests/status/red.png"
-        } else { 
-          self.setField("status", data.status.message);
-          self.d3Render.value = "assests/status/green.png"
-        }
-        updateApp();
-      })
-    }, 10000);
+    }).then(function(data) {
+      if(data.error) {
+        self.setField("status", data.error);
+        self.d3Render.value = "assests/status/red.png"
+      } else { 
+        self.setField("status", data.status.message);
+        self.d3Render.value = "assests/status/green.png"
+      }
+      updateApp();
+    })
   }
 }
 serviceManager.add("ping", Ping);
